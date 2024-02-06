c32a7c22fe8bc03c1c86e60ccf67334caf62177b
Joshua Kelly <joshuakellyeng@gmail.com>
2024-02-06 19:05:20 +0100

    added aria-label and privacy policy

--- js/main.js
+++ js/main.js
@@ -1,65 +1,75 @@
 (function ($) {
-    "use strict";
+	'use strict';
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+	// Spinner
+	var spinner = function () {
+		setTimeout(function () {
+			if ($('#spinner').length > 0) {
+				$('#spinner').removeClass('show');
+			}
+		}, 1);
+	};
+	spinner();
 
+	// Initiate the wowjs
+	new WOW().init();
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('bg-primary shadow-sm').css('top', '0px');
-        } else {
-            $('.sticky-top').removeClass('bg-primary shadow-sm').css('top', '-150px');
-        }
-    });
+	// Sticky Navbar
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 300) {
+			$('.sticky-top').addClass('bg-primary shadow-sm').css('top', '0px');
+		} else {
+			$('.sticky-top').removeClass('bg-primary shadow-sm').css('top', '-150px');
+		}
+	});
 
+	// Facts counter
+	$('[data-toggle="counter-up"]').counterUp({
+		delay: 10,
+		time: 2000,
+	});
 
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+	// Back to top button
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('.back-to-top').fadeIn('slow');
+		} else {
+			$('.back-to-top').fadeOut('slow');
+		}
+	});
+	$('.back-to-top').click(function () {
+		$('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+		return false;
+	});
 
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        items: 1,
-        autoplay: true,
-        smartSpeed: 1000,
-        dots: true,
-        loop: true,
-        nav: true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
-    });
-    
+	// Testimonials carousel
+	$('.testimonial-carousel').owlCarousel({
+		items: 1,
+		autoplay: true,
+		smartSpeed: 1000,
+		dots: true,
+		loop: true,
+		nav: true,
+		navText: [
+			'<i class="bi bi-chevron-left"></i>',
+			'<i class="bi bi-chevron-right"></i>',
+		],
+	});
 })(jQuery);
 
+// MODAL
+const myModal = document.getElementById('myModal');
+const myInput = document.getElementById('myInput');
+
+myModal.addEventListener('shown.bs.modal', () => {
+	myInput.focus();
+});
+
+// ERROR HANDLING 404
+document.addEventListener('DOMContentLoaded', function () {
+	// Check if the requested page exists
+	if (document.body.innerHTML.includes('Page Not Found')) {
+		// Redirect the user to the custom 404 page
+		window.location.href = '404.html';
+	}
+});
